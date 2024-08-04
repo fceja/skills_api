3a3e9ae7ad66b750a9a8ddbbaab1b1e20d79e3a8
fceja <88167774+fceja@users.noreply.github.com>
2024-08-04 15:07:13 -0700

    add post endpoint for creating user

--- api/routes/users_routes.py
+++ api/routes/users_routes.py
@@ -3,12 +3,31 @@ from sqlalchemy.orm import Session
 
 from api.database import get_db
 from api.models.user_model import User
+from api.schemas.user_schema import UserCreate, UserRead
 
 router = APIRouter()
 
 
+# region - create operations
+@router.post("/")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return {"success": True}
+
+
+# endregion - create operations
+
+
+# region - read operations
 @router.get("/")
 def get_user(start: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     users = db.query(User).offset(start).limit(limit).all()
 
     return {"success": True, "users": users}
+
+
+# endregion - read operations
--- api/schemas/user_schema.py
+++ api/schemas/user_schema.py
@@ -4,3 +4,14 @@ from pydantic import BaseModel
 class UserBase(BaseModel):
     name: str
     email: str
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserRead(UserBase):
+    id: int
+
+    class Config:
+        orm_mode: True
