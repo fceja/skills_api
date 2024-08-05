7043a52b7931605db118aac9cc0c2d60be92d5e7
fceja <88167774+fceja@users.noreply.github.com>
2024-08-05 16:34:49 -0700

    add user_language routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 
-from app.routes import languages_routes, users_routes
+from app.routes import languages_routes, users_routes, user_language_routes
 
 # init app
 app = FastAPI()
@@ -9,6 +9,9 @@ app = FastAPI()
 # add routes
 app.include_router(languages_routes.router, prefix="/languages", tags=["languages"])
 app.include_router(users_routes.router, prefix="/users", tags=["users"])
+app.include_router(
+    user_language_routes.router, prefix="/user-languages", tags=["user-languages"]
+)
 
 
 # main entry
--- app/models/user_language_model.py
+++ app/models/user_language_model.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, Table, ForeignKey
+from sqlalchemy import Column, Integer, Table, ForeignKey, UniqueConstraint
 
 from app.models.base import Base
 
 # associative table
 user_language = Table(
-    "user_language",
+    "user_languages",
     Base.metadata,
+    Column("id", Integer, primary_key=True, index=True),
     Column("user_id", Integer, ForeignKey("users.id")),
     Column("language_id", Integer, ForeignKey("languages.id")),
+    UniqueConstraint("user_id", "language_id"),
 )
